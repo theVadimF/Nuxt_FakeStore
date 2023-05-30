a978ce10c30861b6642159845bfa52631f2d3a28
Vadim Faust <vadim40328@gmail.com>
2023-05-30 19:19:55 +0700

    Added an ability to remove items from cart

--- components/CartItem.vue
+++ components/CartItem.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    <p v-if="pending">Loading...</p>
-    <div v-else class="p-3 bg-white rounded-xl shadow-xl item-list grid gap-5">
+    <p v-if="pending" class="w-full h-24 rounded-xl shadow-xl flex items-center justify-center bg-white text-3xl oswald text-gray-500">
+      Loading...
+    </p>
+    <div v-else v-if="product !== null" class="p-3 bg-white rounded-xl shadow-xl item-list grid gap-5">
       <img class="flex-shrink-0 row-span-2 max-h-[100px] justify-self-center self-center image-location" :src="product.image" alt="Product thumbnail">
       <p class="font-medium text-base sm:text-lg row-start-1 col-start-2">{{ product.title }}</p>
       <div class="flex col-end-3 gap-2 items-center mt-auto bottom-location">
         <SpinBox :value="item.quantity" @increase="increase" @decrease="decrease" />
         <p class="text-base sm:text-lg font-light flex">${{ (product.price * item.quantity).toFixed(2) }}</p>
-        <Button class="text-2xl flex items-center ml-auto"><Icon name="la:trash-alt"/></Button>
+        <button class="text-2xl flex items-center ml-auto" @click="$emit('removeItem', item)"><Icon name="la:trash-alt"/></button>
       </div>
     </div>
+    <p v-if="product === null && !pending" class="w-full h-24 rounded-xl shadow-xl flex items-center bg-white text-xl text-red-600 pl-4 box-border">
+      Failed load to product data<br/>Check your internet connection
+    </p>
   </div>
 </template>
 
@@ -22,8 +27,14 @@
   })
 
   const emit = defineEmits();
-
   const {pending, data: product} = useLazyFetch('https://fakestoreapi.com/products/' + props.item.productId);
+  
+  // Update cached price if it differs
+  watch(product, (data) => {
+    if (data !== undefined && data !== null && props.item.cachedPrice != data.price) {
+      props.item.cachedPrice = data.price;
+    }
+  })
 
   function increase() {
     let tmp = props.item.quantity + 1
@@ -37,15 +48,18 @@
     if (tmp > 0) {
       props.item.quantity = tmp;
     } else {
-      emit('removeId', props.item.productId);
+      emit('removeItem', props.item);
     }
   }
-
 </script>
 
 <style scoped>
   .item-list {
     grid-template-columns: 100px 1fr;
+  }
+
+  .oswald {
+    font-family: Oswald;
   }
 
   .bottom-location {
--- components/CartMenu.vue
+++ components/CartMenu.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="fixed bg-black z-10 h-full w-full top-0 bg-opacity-25" @click="$emit('closeCart', false)"></div>
-  <div class="fixed bg-white z-20 w-full max-w-xl h-full right-0 top-0 flex flex-col box-border p-3 gap-3">
-    <div class="flex justify-between items-center text-3xl">
+  <div class="fixed bg-white z-20 w-full max-w-xl h-full right-0 top-0 flex flex-col box-border">
+    <div class="flex justify-between items-center text-3xl p-3">
       <h1 class="headtext">Cart</h1>
       <button @click="$emit('closeCart', false)"><Icon alt="Close cart" name="material-symbols:close"/></button>
     </div>
-    <div class="flex flex-col gap-3 h-full overflow-y-auto">
+    <div class="flex flex-col gap-3 h-full overflow-y-auto box-border p-3">
       <p class="w-full h-full flex justify-center items-center headtext text-5xl text-gray-600" v-if="cart.length === 0">Empty</p>
-      <CartItem v-for="product in cart" :item="product"/>
+      <CartItem v-for="product in cart" :item="product" @removeItem="(item) => removeItem(item)" :key="product.productId"/>
     </div>
-    <div class="flex items-center gap-2">
-      <button :disabled="true" class="flex items-center px-3 py-1 gap-1 rounded-xl shadow-md" :class="cart.length === 0 ? 'cursor-not-allowed bg-gray-600 text-gray-200' : 'bg-green-800 text-white'"><Icon class="self-center text-xl" name="la:shopping-bag"/>Checkout</button>
+    <div class="flex items-center gap-2 p-3">
+      <button :disabled="cart.length === 0" class="flex items-center px-3 py-1 gap-1 rounded-xl shadow-md" :class="cart.length === 0 ? 'cursor-not-allowed bg-gray-600 text-gray-200' : 'bg-green-800 text-white'"><Icon class="self-center text-xl" name="la:shopping-bag"/>Checkout</button>
       <p class="text-xl font-light">${{ calcTotal() }}</p>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { remove } from '@vue/shared';
 import { defineComponent } from 'vue'
 
 export default defineComponent({
@@ -37,7 +38,10 @@ export default defineComponent({
         total += element.cachedPrice * element.quantity;
       });
       return total.toFixed(2);
-    }
+    },
+    removeItem(obj: object) {
+      this.cart.splice(this.cart.indexOf(obj), 1);
+    } 
   }
 })
 </script>
